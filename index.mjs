2165d144fd1f64034e9de6ba369280c3b66baef0
Eduardo Saavedra <eduardo.saavedra@pluto.tv>
2021-05-11 19:55:06 -0500

    setup

---
+++ index.mjs
@@ -0,0 +1,16 @@
+import { gql, ApolloServer } from 'apollo-server'
+import gapiToGraphQL from 'gapi-to-graphql'
+import YouTubeAPI from 'gapi-to-graphql/google_apis/youtube-v3.js'
+
+const { schema, resolvers } = gapiToGraphQL({ gapiAsJsonSchema: YouTubeAPI })
+
+const server = new ApolloServer({
+  typeDefs: gql`
+    ${schema}
+  `,
+  resolvers
+})
+
+server.listen({ port: 3031 }).then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`)
+})
